13263a7bf42f70681395caec50fc3a0d2ecdaa85
kozak201201 <kozak20012001@gmail.com>
2021-06-14 19:45:11 +0300

    add express-validator to packege json and create authStudentRouter

--- controllers/authStudentController.js
+++ controllers/authStudentController.js
@@ -1,12 +1,17 @@
 const Student = require('../models/student');
+const Role = require('../models/role');
 const bcrypt = require('bcryptjs');
 
 const authStudentController = {
     registration: function(req, res) {
-        const {email, password} = req.body;
+        const studentRole = new Role({value: 'Student'});
+        const adminRole = new Role({value: 'Admin'});
+        // const {email, password} = req.body;
 
-        if (Student.findOne({email})) return res.sendStatus(409);
+        // if (Student.findOne({email})) return res.sendStatus(409);
         
-        const hashPass = bcrypt.hashSync(password, 4);
+        // const hashPass = bcrypt.hashSync(password, 4);
     }
 }
+
+module.exports = authStudentController;
---
+++ routers/authStudentRouter.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const controller = require('../controllers/authStudentController');
+const { check } = require('express-validator');
+
+const router = express.Router();
+
+router.post('/registration',[
+    check('email', 'Invalid email').isEmail(),
+    check('password', 'Pussword must be more then 5 symbols').isLength({min: 6})
+], controller.registration);
