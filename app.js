c8cd1e70bef0b4d236ae2c9457bc1016fffc0353
kozak201201 <kozak20012001@gmail.com>
2021-06-14 20:21:59 +0300

    add authRouter.js to app.js

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+const authRouter = require('./routers/authStudentRouter');
 
 const uri = "mongodb+srv://kozak:kozak@cluster0.hmsow.mongodb.net/auth_roles?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use('/auth', authRouter);
 
 async function start() {
     try {
