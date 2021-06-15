e4fba5787db08ad650c3640dee46e656680dff29
kozak201201 <kozak20012001@gmail.com>
2021-06-15 05:36:25 +0300

    create login method

--- controllers/authStudentController.js
+++ controllers/authStudentController.js
@@ -1,6 +1,8 @@
 const Student = require('../models/student');
 const Role = require('../models/role');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 const authStudentController = {
     registration: async function(req, res) {
@@ -20,6 +22,20 @@ const authStudentController = {
             res.status(404).send(err);
         })
     },
+    login: async function(req, res) {
+        const {email, password} = req.body;
+        const student = await Student.findOne({email});
+
+        if (!student) return res.sendStatus(404);
+
+        const hashPassword = bcrypt.hashSync(password, 4);
+        const isValidPassword = bcrypt.compareSync(password, hashPassword);
+
+        if (!isValidPassword) return res.status(400).send({message: 'Error: Invalid password'});
+
+        const token = jwt.sign({email: student.email, roles: student.roles}, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '15m'});
+        res.status(200).send({token});
+    }
 }
 
 module.exports = authStudentController;
